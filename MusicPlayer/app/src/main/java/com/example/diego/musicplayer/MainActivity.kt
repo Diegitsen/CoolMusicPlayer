bb10e1c80a2c30054b6fcdb2843265039f30af50
diegitsen <diegitsen@gmail.com>
2017-12-30 23:56:08 -0500

    oye sii

--- MusicPlayer/app/src/main/java/com/example/diego/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/com/example/diego/musicplayer/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 else
-                {
+                {//oye sii
                     try
                     {
                         mp!!.setDataSource(song.songURL)
